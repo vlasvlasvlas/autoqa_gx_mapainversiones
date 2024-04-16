5e4e0d89eb62f152ce01356d1bcf970791c13d76
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2024-04-16 01:00:23 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# autoqa_gx_mapainversiones
- automatizar QA con Great Expectations sobre SQLServer
+# Proyecto de Validación de Datos con Great Expectations
+
+## Descripción
+
+Este proyecto es un prototipo que utiliza la biblioteca Great Expectations para automatizar el control de calidad de datos durante la carga de nuevos datos mediante procesos ETL. Se configura para generar informes HTML que documentan los resultados de las validaciones realizadas en un entorno de base de datos SQL, asegurando la integridad y calidad de los datos cargados.
+
+## Configuración del Entorno
+
+Antes de iniciar, asegúrate de que las variables de entorno estén configuradas correctamente en tu archivo `.env`:
+
+```plaintext
+SQL_CONNSTRING_170_PRE="mssql+pyodbc://<usuario>:<contraseña>@<host>,<puerto>/"
+SQL_CONNSTRING_170_POST="?driver=ODBC+Driver+17+for+SQL+Server&autocommit=true"
+SQL_CONNSTRING_170_DB=<dbname>
+```
+
+**Nota:** Sustituye `<usuario>`, `<contraseña>`, `<host>`, `<puerto>` y `<dbname>` con tus credenciales reales y la información del servidor.
+
+## Instalación
+
+Para ejecutar las validaciones, debes seguir estos pasos:
+
+1. Instala las dependencias necesarias: `pip install great_expectations`.
+2. Ejecuta el script principal (en la carpeta de scripts) para iniciar las validaciones y generar la documentación de datos.
+
+## Componentes del Proyecto
+
+### Conexión a la Fuente de Datos
+
+Establecimiento de una conexión a una base de datos SQL y configuración de una fuente de datos dentro del contexto de Great Expectations.
+
+### Creación y Ejecución de Expectativas
+
+Definición y configuración de expectativas específicas para validar:
+
+- Presencia de valores nulos.
+- Unicidad de valores en las columnas.
+- Rangos específicos de valores (fechas, números, etc.).
+
+### Checkpoints
+
+Configuración y ejecución de checkpoints para validar los datos contra las expectativas definidas y documentar los resultados.
+
+## Expansión de Reglas y Expectativas
+
+### Ideas para Nuevas Reglas
+
+- **Consistencia Temporal:** Validar que las fechas en los registros sean realistas y se mantengan dentro de un rango lógico.
+- **Validación de Relaciones:** Asegurar que las claves foráneas correspondan a registros válidos en otras tablas.
+- **Patrones de Datos:** Implementar expectativas para validar formatos específicos como correos electrónicos o números de teléfono.
+- **Análisis de Tendencias:** Detectar cambios significativos que podrían indicar problemas de calidad de los datos.
+
+### Automatización de Pruebas
+
+Integración de estas validaciones en un pipeline de CI/CD para ejecutar automáticamente las pruebas de datos con cada actualización de la base de datos o cambio en las definiciones de expectativas.
+
+## Contribuciones
+
+Te invitamos a contribuir al proyecto. Si tienes sugerencias o mejoras, crea un pull request o abre un issue en el repositorio del proyecto.
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT. Consulta el archivo `LICENSE.md` para más detalles.
+
+# Fuentes Documentales
+
+- https://docs.greatexpectations.io/docs/oss/
+- https://www.paradigmadigital.com/dev/great-expectations-data-assistant/ 
---
+++ scripts/pa_qa.py
@@ -0,0 +1,150 @@
+# GX
+import great_expectations as gx
+from great_expectations.checkpoint import Checkpoint
+from great_expectations.data_context.types.resource_identifiers import (
+    ValidationResultIdentifier,
+)
+from great_expectations.expectations.expectation import ExpectationConfiguration
+
+# ENV
+import os, sys
+from dotenv import load_dotenv
+from datetime import datetime, timezone
+
+# Load environment variables
+load_dotenv()
+
+
+# Data Vars:
+# db connection string
+db = os.getenv("SQL_CONNSTRING_DB")
+datasource_name = "sql_pan_stg"
+my_checkpoint_name = "my_sql_checkpoint"
+
+# Load the gx context
+# The context serves as an entry point for GX operations and holds all relevant configurations.
+context = gx.get_context()
+
+connection_string = (
+    os.getenv("SQL_CONNSTRING_PRE") + db + os.getenv("SQL_CONNSTRING_POST")
+)
+
+print(connection_string)
+
+# doc:
+# https://docs.greatexpectations.io/docs/oss/guides/connecting_to_your_data/fluent/database/connect_sql_source_data/
+
+# create a SQL Data Source :
+# add your dataset to this context as a Data Source to start interacting with the data.
+# datasource_sql_pan_stg = context.sources.add_sql(
+#     name="sql_pan_stg", connection_string=connection_string
+# )
+
+datasources = context.list_datasources()
+datasource_names = [ds["name"] for ds in datasources]
+
+if datasource_name not in datasource_names:
+
+    print(f"Datasource {datasource_name} does not exist.")
+    # create a SQL Data Source :
+    # add your dataset to this context as a Data Source to start interacting with the data.
+    datasource_sql_pan_stg = context.sources.add_sql(
+        name=datasource_name, connection_string=connection_string
+    )
+
+    # data assets
+    # Define Data Assets to specify the subset of data you'd like to work with. The asset can be as simple as full tables, or be as complex as a custom Data Analysis Expressions (DAX) query.
+
+    # check tabla
+    asset_name = "asset_1_pan_stg_ejecucion"
+    asset_table_schema = "dbo"
+    asset_table_name = "mef_vw_ejecucion"
+
+    # add table asset
+    table_asset = datasource_sql_pan_stg.add_table_asset(
+        name=asset_name, schema_name=asset_table_schema, table_name=asset_table_name
+    )
+
+    # DAX
+    # If you'd like to define your own measures or have more control over specific rows, you can add a DAX asset with a custom DAX query.
+
+    # build batch request
+    # https://docs.greatexpectations.io/docs/reference/learn/terms/batch_request/
+    batch_request = datasource_sql_pan_stg.get_asset(
+        asset_name
+    ).build_batch_request()
+
+    print(batch_request)
+
+    # Expectation suite
+    # In order to add specific constraints to the assets, you first have to configure Expectation Suites. After adding individual Expectations to each suite, you can then update the Data Context set up in the beginning with the new suite.
+
+    # suite name
+    expectation_suite_name = "expectativas_pan_stg_ejecucion"
+
+    suite_store = context.add_expectation_suite(expectation_suite_name)
+
+    # add expectations:
+    # https://greatexpectations.io/expectations
+
+    # 1. expect_column_values_to_not_be_null
+    suite_store.add_expectation(
+        ExpectationConfiguration(
+            "expect_column_values_to_not_be_null", {"column": "anio"}
+        )
+    )
+
+    context.add_or_update_expectation_suite(expectation_suite=suite_store)
+
+    # Optional:
+    # Run assert "my_expectation_suite" in context.list_expectation_suite_names() to veriify the Expectation Suite was created.
+
+    validator = context.get_validator(
+        batch_request=batch_request,
+        expectation_suite_name=expectation_suite_name,
+    )
+
+    validator.expect_column_values_to_not_be_null(column="gasto")
+
+    validator.head()
+
+    # Run the following command to save your Expectation Suite
+    validator.save_expectation_suite(discard_failed_expectations=False)
+
+    context = context.convert_to_file_context()
+
+    # Validate your data
+    # You'll create and store a Checkpoint for your Batch, which you can use to validate and run post-validation actions.
+
+    checkpoint = Checkpoint(
+        name=my_checkpoint_name,
+        run_name_template="%Y%m%d-%H%M%S-my-run-name-template",
+        data_context=context,
+        batch_request=batch_request,
+        expectation_suite_name=expectation_suite_name,
+        action_list=[
+            {
+                "name": "store_validation_result",
+                "action": {"class_name": "StoreValidationResultAction"},
+            },
+            {
+                "name": "update_data_docs",
+                "action": {"class_name": "UpdateDataDocsAction"},
+            },
+        ],
+    )
+
+    # save checkpoint
+    context.add_or_update_checkpoint(checkpoint=checkpoint)
+
+else:
+    print(f"Datasource {datasource_name} exists.")
+    # verify the Expectation Suite was created:
+    print("verify the Expectation Suite was created:")
+    print(context.list_expectation_suite_names())
+    checkpoint = context.get_checkpoint(my_checkpoint_name)
+
+
+# run checkpoint
+checkpoint.run()
+context.open_data_docs()
